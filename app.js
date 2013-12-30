e61765c157917d6147eb809c174503d820ce4040
Brian Gouws - work <briangouws@gmail.com>
2013-12-30 16:17:54 +0000

    socket test

--- app.js
+++ app.js
@@ -6,15 +6,23 @@ var app = require('http').createServer(),
 
 app.listen(443);
 
+/// zeromq php style logic
+// $pull = $context->getSocket(ZMQ::SOCKET_PULL);
+// $pull->bind('tcp://127.0.0.1:5555'); // Binding to 127.0.0.1 means the only client that can connect is itself
+// $pull->on('message', array($pusher, 'onBlogEntry'));
+
+zmq.connect('tcp://127.0.0.1:5555');
 
 io.sockets.on('connection', function (socket) {
 //  socket.emit('news', { hello: 'world' });
 
+    socket.volatile.emit('news', {hello : 'some news that you may or may not get'});
 
-  socket.volatile.emit('news', {hello : 'some news that you may or may not get'});
+    socket.on('my other event', function (data) {
+      console.log(data);
+    });
+});
 
-
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+zmq.on('message', function(data){
+    socket.volatile.emit('stream', {data : data});
 });
