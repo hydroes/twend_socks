3fc78a2e9c7251c936e01bdfaee860ec22f97671
Brian Gouws - work <briangouws@gmail.com>
2013-12-30 15:56:51 +0000

    volatile message testing

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 var app = require('http').createServer(),
     io = require('socket.io').listen(app),
     fs = require('fs'),
-    zmq = require("zmq")
+    zmq = require("zmq");
 
 
 app.listen(443);
 
 
 io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
+//  socket.emit('news', { hello: 'world' });
+
+
+  socket.volatile.emit('news', {hello : 'some news that you may or may not get'});
+
+
   socket.on('my other event', function (data) {
     console.log(data);
   });
