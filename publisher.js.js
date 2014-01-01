170fdd4d925fe5f2f82f1e8746312d84e6fd71dd
root <root@vps.servermania.com>
2014-01-01 14:17:31 +0000

    added counter

--- publisher.js.js
+++ publisher.js.js
@@ -4,7 +4,11 @@ var zmq = require('zmq')
 sock.bindSync('tcp://127.0.0.1:3000');
 console.log('Producer bound to port 3000');
 
+var count = 0;
+
 setInterval(function(){
-  console.log('sending work');
-  sock.send('some work');
+  console.log('sending work ' + count);
+  sock.send('some work ' + count);
+  count++;
+
 }, 500);
