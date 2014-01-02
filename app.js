fa3e5f9f574cdf522f480ea3d79323f21c267083
Brian Gouws - work <briangouws@gmail.com>
2014-01-02 11:50:04 +0000

    socket testing

--- app.js
+++ app.js
@@ -6,28 +6,16 @@ var app = require('http').createServer(),
 
 app.listen(443);
 
-/// zeromq php style logic
-// $pull = $context->getSocket(ZMQ::SOCKET_PULL);
-// $pull->bind('tcp://127.0.0.1:5555'); // Binding to 127.0.0.1 means the only client that can connect is itself
-// $pull->on('message', array($pusher, 'onBlogEntry'));
-
-console.log("Current 0MQ version is " + zmq.version);
-
-zmqSocket = zmq.socket("sub");
-
-
 //console.log(zmqSocket)
-zmqSocket.connect('tcp://127.0.0.1:5555');
-zmqSocket.subscribe('');
+zmqSocket.connect('tcp://127.0.0.1:3000');
 
 
 io.sockets.on('connection', function (socket) {
 //  socket.emit('news', { hello: 'world' });
 
-    zmqSocket.on('message', function(data) {
+    zmqSocket.on('message', function(msg) {
         console.log('tweets bru!!');
-//            socket.volatile.emit('stream', {data : data});
-            sockets.emit('stream', {data : data});
+            sockets.emit('stream', {data : msg.toString()});
     });
 
     socket.volatile.emit('news', {hello : 'some news that you may or may not get'});
--- npub.php
+++ npub.php
@@ -5,7 +5,6 @@ $context = new ZMQContext();
 //  Socket to send messages on
 $socket = new ZMQSocket($context, ZMQ::SOCKET_PUSH);
 
-//If I want to connect to the server I use this line
 $socket->bind('tcp://127.0.0.1:3000');
 
 $count = 0;
