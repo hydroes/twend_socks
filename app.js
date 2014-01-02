e0f81099df9111d6eac0b979cad770e1b402d1de
Brian Gouws - work <briangouws@gmail.com>
2014-01-02 16:04:04 +0000

    socket testing

--- app.js
+++ app.js
@@ -9,20 +9,31 @@ app.listen(443);
 //console.log(zmqSocket)
 zmqSocket.connect('tcp://127.0.0.1:3000');
 
-
+var clientScockets = Array();
 io.sockets.on('connection', function (socket) {
-//  socket.emit('news', { hello: 'world' });
 
-    zmqSocket.on('message', function(msg) {
-        console.log('tweets bru!!');
-            sockets.emit('stream', {data : msg.toString()});
-    });
+//    clientScockets[] = socket;
+//    zmqSocket.on('message', function(msg) {
+//        socket.volatile.emit('stream', {data : msg.toString()});
+//    });
 
-    socket.volatile.emit('news', {hello : 'some news that you may or may not get'});
+    socket.volatile.emit('news', {hello : 'you are connected'});
+//
+//    socket.on('my other event', function (data) {
+//      console.log(data);
+//    });
+});
 
-    socket.on('my other event', function (data) {
-      console.log(data);
-    });
+
+zmqSocket.on('message', function(msg) {
+    console.log('tweets bru!!');
+//            sockets.emit('stream', {data : msg.toString()});
+
+//        socket.volatile.emit('stream', {data : msg.toString()});
+
+        // an event sent to all connected clients
+        io.sockets.emit(msg.toString());
+
 });
 
 
