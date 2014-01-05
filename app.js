b30fadff7a7eb7282b1e8fc86c1c4e0d22e16dff
Brian Gouws <briangouws@gmail.com>
2014-01-05 13:42:37 +0000

    socket testing

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ io.sockets.on('connection', function (socket) {
     // store socket for later use
     sockets.push(socket);
 
-    socket.on('disconnect', function (socket) {
+    socket.on('disconnect', function () {
         console.log('disconected' + socket.id);
         io.sockets.emit('user disconnected');
 
@@ -29,9 +29,9 @@ zmqSocket.on('message', function(msg) {
     if (sockets.length !== 0) {
         for (i = 0; i < sockets.length; i++) {
             var sock = sockets[i];
-            console.log(sock.connected)
+//            console.log(sock.connected)
 //            sock.volatile.emit('tweet', msg.toString());
         }
     }
-    console.log(sockets.length)
+//    console.log(sockets.length)
 });
