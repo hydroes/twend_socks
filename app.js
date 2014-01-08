c097217b441acb274a4d405a1a3ad5cb52837b05
Brian Gouws - work <briangouws@gmail.com>
2014-01-08 16:48:19 +0000

    cleaning up

--- app.js
+++ app.js
@@ -8,24 +8,14 @@ app.listen(443);
 var zmqSocket = zmq.socket('pull');
 zmqSocket.connect('tcp://127.0.0.1:3000');
 
-var sockets = new Array();
-
 io.sockets.on('connection', function (socket) {
-    // store socket for later use
-    sockets.push(socket);
 
     socket.on('disconnect', function () {
         console.log('user disconnected');
-
-        // remove socket from array
-        sockets.splice(sockets.indexOf(socket), 1);
-
     });
 
     socket.on('error', function () {
         console.log('an error occurred');
-        // remove socket from array
-        sockets.splice(sockets.indexOf(socket), 1);
     });
 });
 
