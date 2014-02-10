00e70a2ca14c90550f22853fcf0fe8b0f3149388
Brian Gouws <briangouws@gmail.com>
2014-02-10 21:49:21 +0000

    fixing pause feed logic

--- app.js
+++ app.js
@@ -50,10 +50,11 @@ zmqSocket.on('message', function(msg)
     for (var socketId in io.sockets.sockets)
     {
         var feed_paused = io.sockets.sockets[socketId].get('feed_flow_status', function (err, name) {
+            return name;
             console.log('Chat message by ', name);
         });
-        console.log('feedpaused:');
-        console.log(feed_paused);
+//        console.log('-------feedpaused:');
+//        console.log(feed_paused);
         if (feed_paused === false) {
             continue;
         }
