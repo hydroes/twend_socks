b10338ea5c754a280b8832d2ac8788fb7e72474f
Brian Gouws <hydroes@gmail.com>
2014-04-16 11:49:13 +0100

    added redis test file

--- test_redis.js
+++ test_redis.js
@@ -2,7 +2,7 @@ var redis = require('node-redis')
 
 var client = redis.createClient(6379);
 
-client.get('laravel:last_minute_aggregated', function (error, buffer)
+client.get('laravel:last_minute_total', function (error, buffer)
 {
     console.log("error:\n" + error);
 
